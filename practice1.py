b095379eaeb1e3a447786bcd81570a984598c90a
royalharry007 <harichandchauhan78@gmail.com>
2024-05-15 15:35:15 +0200

    practice problem1

---
+++ practice1.py
@@ -0,0 +1,10 @@
+age = int(input("enter the your age "))
+
+if age<13:
+    print("your are child")
+elif age>=13 and age<19:
+    print("your are teenger")
+elif (age>=19) and (age<59):
+    print("your are adult")
+else:
+    print("you are senior")
