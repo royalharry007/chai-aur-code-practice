8afe53940c7d6153a595108563fcdff4817e6c79
royalharry007 <harichandchauhan78@gmail.com>
2024-05-16 05:50:06 +0200

    lambda function

---
+++ function_problem/lambda_function.py
@@ -0,0 +1,5 @@
+# write a lambda function which give the cube of number 
+
+cube = lambda x: x**3   #  a lambda function is the function without name 
+print(cube(5))           # it is mostly used for single function which can be done in single line 
+
